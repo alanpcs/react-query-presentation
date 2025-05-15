150466af7182d7fd5ffb39c05721c8b2207e0139
AlanPCS <alanpcsilva@gmail.com>
2025-05-15 16:42:30 -0300

    feat: shows devtools 'in prod'

--- src/AppProviders.tsx
+++ src/AppProviders.tsx
@@ -1,11 +1,17 @@
+import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { ThemeProvider } from "./styles/ThemeProvider";
 import { useMatchRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 const queryClient = new QueryClient();
+
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import("@tanstack/react-query-devtools/build/modern/production.js").then((d) => ({
+    default: d.ReactQueryDevtools,
+  })),
+);
 
 export const AppProviders = ({ children }: React.PropsWithChildren) => {
   const matchRoute = useMatchRoute();
@@ -20,7 +26,11 @@ export const AppProviders = ({ children }: React.PropsWithChildren) => {
         <GlobalStyle />
         {children}
       </ThemeProvider>
-      {shouldShowDevTools && <TanStackRouterDevtools />}
+      {shouldShowDevTools && (
+        <React.Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </React.Suspense>
+      )}
     </QueryClientProvider>
   );
 };
